2931d6f5482194a31a07c6782567fc2661be2b11
EequalsMC2 <zhongxiao0711@gmail.com>
2021-01-19 19:23:36 +0800

    move configure to py_config.hpp

--- include/py_config.hpp
+++ include/py_config.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <mem_literals.hpp>
 #include <vector>
-namespace shmpy
-{
-    size_t CacheBinEps = 1024;
-    std::vector<size_t> BatchBinSize;
-    std::vector<size_t> BatchBinCount;
+
+namespace shmpy::config {
+///
+/// \brief When memory required size is less than CacheBinEps, buffer will be
+/// stored in cache bin
+///
+size_t CacheBinEps = 1_KB;
+
+std::vector<size_t> BatchBinSize{ 512,   1_KB,  2_KB,  4_KB,  8_KB,
+                                  12_KB, 16_KB, 24_KB, 32_KB, 48_KB,
+                                  64_KB, 80_KB, 96_KB, 128_KB };
+
+std::vector<size_t> BatchBinCount{ 64, 64, 64, 64, 64, 64, 64,
+                                   64, 50, 50, 50, 50, 32, 32 };
+
+size_t InstantBinEps = BatchBinSize.back() * 8;
 }
