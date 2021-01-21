c712860941b50d74047a38366eb18f6373cd457c
EequalsMC2 <zhongxiao0711@gmail.com>
2021-01-21 15:25:40 +0800

    add constructor for variable_desc

--- include/py_variable.hpp
+++ include/py_variable.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "py_dtype.hpp"
+#include "py_message.hpp"
 #include "shared_memory.hpp"
 
 #include <segment.hpp>
@@ -37,17 +38,42 @@ protected:
 };
 
 /**
- * @brief 只保存在服务器端
+ * \brief 只保存在服务器端
  *
  */
 class variable_desc : public base_variable
 {
+
+  friend class Py_Server;
+
 private:
   std::string_view name;
   std::atomic_uint32_t ref_count;
   std::list<size_t> attach_ids;
   std::mutex mtx;
   std::shared_ptr<shm_kernel::memory_manager::base_segment> segment;
+
+public:
+  variable_desc(const shm_kernel::message_handler::msg_head* msg_head,
+                const REQ_InsertVariable* req,
+                std::shared_ptr<shm_kernel::memory_manager::base_segment>)
+  {
+    this->dtype = req->dtype;
+    this->size = req->size;
+    this->is_py_buff_protocol = req->is_pybuff_protocol;
+    this->segment = segment;
+    this->ref_count = 1;
+    this->attach_ids.push_back(msg_head->from);
+
+    //    switch (typeid(*segment.get())) {
+    //      case typeid(shm_kernel::memory_manager::cache_segment):
+    //        break;
+    //      case typeid(shm_kernel::memory_manager::static_segment):
+    //        break;
+    //      case typeid(shm_kernel::memory_manager::instant_segment):
+    //        break;
+    //    }
+  }
 };
 
 class attached_variable : public base_variable
