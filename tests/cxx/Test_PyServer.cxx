c8dd37f1bff1ef57c22f0745b00e4716b221c426
EequalsMC2 <zhongxiao0711@gmail.com>
2021-02-04 20:55:32 +0800

    sync: ...

--- tests/cxx/Test_PyServer.cxx
+++ tests/cxx/Test_PyServer.cxx
@@ -56,7 +56,7 @@ TEST_CASE("服务器先被Delete", "[PyServer]")
 TEST_CASE("Insert string", "[PyServer]")
 {
   auto svr = shmpy::Py_Server("test5");
-  svr.Py_StrInsert("msg", "Hello World");
+  svr.Py_StrInsert("msg", "Hello World",;
   auto __msg = svr.Py_StrGet("msg");
   svr.Py_StrSet("msg", "Good");
 }
